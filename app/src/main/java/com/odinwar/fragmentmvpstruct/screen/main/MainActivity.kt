2cb5e28103c17167e7506046336953a29d8b0965
nvqquy98 <nvqquy98@gmail.com>
2020-11-06 11:38:54 +0700

    Init Project

---
+++ app/src/main/java/com/odinwar/fragmentmvpstruct/screen/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.odinwar.fragmentmvpstruct.screen.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.odinwar.fragmentmvpstruct.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
